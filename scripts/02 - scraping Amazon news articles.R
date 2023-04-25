4d4a194806f74bc5add38f35ef4a50c8ddd40f7c
simonweigold <simonw750@gmail.com>
2023-04-25 21:31:50 +0200

    save data as data frame and export csv.

--- scripts/02 - scraping Amazon news articles.R
+++ scripts/02 - scraping Amazon news articles.R
@@ -9,3 +9,8 @@ gu_api_key()
 api_request <- gu_content(query = "amazon",
                           from_date = "2000-01-01",
                           to_date = "2022-12-31")
+
+# Save data as data frame
+df <- api_request %>% select(-tags)
+df <- as.data.frame(df)
+write.csv(df, here::here("guardian_amazon.csv"))
