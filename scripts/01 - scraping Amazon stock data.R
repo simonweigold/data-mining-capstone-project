404c3ae1c6d29023903d66d7ee922a48a3f6c0f1
simonweigold <simonw750@gmail.com>
2023-04-25 15:58:49 +0200

    added definition of API key and request URL.

--- scripts/01 - scraping Amazon stock data.R
+++ scripts/01 - scraping Amazon stock data.R
@@ -6,3 +6,10 @@ library(here)
 setwd(here::here())
 library(rstudioapi)
 
+# Set API key and request URL
+api_key <- rstudioapi::askForPassword()
+request_url <- str_c("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=",
+                     "AMZN",
+                     "&outputsize=full&apikey=",
+                     api_key)
+
