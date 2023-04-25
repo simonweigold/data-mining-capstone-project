e9f4e1f3f59f32eaa79f0c7b8c454757c984b9ca
simonweigold <simonw750@gmail.com>
2023-04-25 21:31:15 +0200

    use the wrapper function to retrieve data.

--- scripts/02 - scraping Amazon news articles.R
+++ scripts/02 - scraping Amazon news articles.R
@@ -4,3 +4,8 @@ library(guardianapi)
 
 # Load API key
 gu_api_key()
+
+# GET request
+api_request <- gu_content(query = "amazon",
+                          from_date = "2000-01-01",
+                          to_date = "2022-12-31")
