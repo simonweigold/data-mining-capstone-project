f79b755d074b4cfbe9f6f2726b457ba2263f870e
simonweigold <simonw750@gmail.com>
2023-04-26 15:46:36 +0200

    calculated the average sentiment per day.

--- scripts/04 - analysis.R
+++ scripts/04 - analysis.R
@@ -8,3 +8,9 @@ VADER <- import(here::here("ga_clean_VADER.csv"))
 VADER$date <- as.Date(VADER$web_publication_date)
 # Stock data
 stock <- import(here::here("amzn.csv"))
+
+# Average sentiment per day
+# VADER data preparation
+df_means_VADER <- VADER %>%
+  group_by(date) %>%
+  summarize(mean_compound = mean(compound))
