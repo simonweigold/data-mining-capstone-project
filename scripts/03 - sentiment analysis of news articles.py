18b173f09a38f5c12989096e7a2e0c548429d218
simonweigold <simonw750@gmail.com>
2023-04-26 15:27:36 +0200

    added Python code which conducts the sentiment analysis using VADER from NLTK.

---
+++ scripts/03 - sentiment analysis of news articles.py
@@ -0,0 +1,29 @@
+import os
+#nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+
+sid = SentimentIntensityAnalyzer()
+
+# Import data
+df = pd.read_csv(os.path.join(os.getcwd(), "ga_clean.csv"))
+
+# Calculate scores
+df['scores'] = df['headline'].apply(lambda review: sid.polarity_scores(review))
+
+df['compound']  = df['scores'].apply(lambda score_dict: score_dict['compound'])
+
+def categorize(value):
+    if value >= 0.05:
+        return 'pos'
+    elif value <= -0.05:
+        return 'neg'
+    else:
+        return 'neu'
+df['comp_score'] = df['compound'].apply(categorize)
+
+# Show head of results
+print(df.head())
+
+# Export csv
+df.to_csv(os.path.join(os.getcwd(), "ga_clean_VADER.csv"), index = False)
