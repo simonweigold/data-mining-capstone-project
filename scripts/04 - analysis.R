b52b9c4890819d5e25c8131de22c63e333f50981
simonweigold <simonw750@gmail.com>
2023-04-26 16:13:03 +0200

    corrected stock data for stock split and added visualisation of stock data over time.

--- scripts/04 - analysis.R
+++ scripts/04 - analysis.R
@@ -15,7 +15,7 @@ df_means_VADER <- VADER %>%
   group_by(date) %>%
   summarize(mean_compound = mean(compound))
 
-# VADER visualisation
+# VADER visualisation over time
 df_means_VADER %>% 
   ggplot() +
   geom_line(aes(x=date, y=mean_compound), col="dodgerblue4") +
@@ -38,3 +38,33 @@ df_means_VADER %>%
         legend.text = element_text(color="black", size=12, family="serif"),
         text = element_text(color="black", size=14, family="serif")
   )
+
+# Correct stock data for stock split in June 2022
+stock$value <- stock$close_data
+stock$value[stock$date > '2022-06-03'] <- stock$value*20
+
+# Visualisation of stock data over time
+stock %>% 
+  ggplot() +
+  geom_line(aes(x=date, y=value), col="dodgerblue4") +
+  xlab("Date") +
+  ylab("Value") +
+  scale_x_date(date_minor_breaks = "1 day") +
+  theme(axis.text = element_text(color="black", size=12, family="serif"),
+        axis.text.x = element_text(color="black", size=12, family="serif"),
+        axis.ticks.x = element_line(),
+        axis.ticks.y = element_line(),
+        axis.line.x = element_line(size=0.5, color="grey"),
+        axis.line.y = element_line(size=0.5, color="grey"),
+        panel.grid = element_line(color = "honeydew2",
+                                  size = 0.5,
+                                  linetype = 1),
+        panel.background = element_rect(fill="white"),
+        plot.margin = margin(10,10,10,10),
+        plot.title = element_text(color="black", size=16, family="serif"),
+        plot.subtitle = element_text(color="grey26", size=14, family="serif"),
+        legend.text = element_text(color="black", size=12, family="serif"),
+        text = element_text(color="black", size=14, family="serif")
+  )
+
+# Merge stock values and sentiment scores for dates
