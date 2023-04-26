47474f610dd3bfe414d9ddcfae51dd929e6bbcb0
simonweigold <simonw750@gmail.com>
2023-04-26 15:45:43 +0200

    imported data for analysis.

--- scripts/04 - analysis.R
+++ scripts/04 - analysis.R
@@ -2,3 +2,9 @@ library(rio)
 library(here)
 library(lubridate)
 
+# Import data
+# News headlines with SA scores
+VADER <- import(here::here("ga_clean_VADER.csv"))
+VADER$date <- as.Date(VADER$web_publication_date)
+# Stock data
+stock <- import(here::here("amzn.csv"))
