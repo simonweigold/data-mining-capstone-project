bcd17b333daa89effe54d13230c0d336deac38a8
simonweigold <simonw750@gmail.com>
2023-04-27 20:31:14 +0200

    defined custom theme as a separate object to simplify visualisation code.

--- scripts/04 - analysis.R
+++ scripts/04 - analysis.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(rio)
 library(here)
 library(lubridate)
@@ -9,6 +10,24 @@ VADER$date <- as.Date(VADER$web_publication_date)
 # Stock data
 stock <- import(here::here("amzn.csv"))
 
+# Define custom theme
+custom_theme <-   theme(axis.text = element_text(color="black", size=12, family="serif"),
+                        axis.text.x = element_text(color="black", size=12, family="serif"),
+                        axis.ticks.x = element_line(),
+                        axis.ticks.y = element_line(),
+                        axis.line.x = element_line(size=0.5, color="grey"),
+                        axis.line.y = element_line(size=0.5, color="grey"),
+                        panel.grid = element_line(color = "honeydew2",
+                                                  size = 0.5,
+                                                  linetype = 1),
+                        panel.background = element_rect(fill="white"),
+                        plot.margin = margin(10,10,10,10),
+                        plot.title = element_text(color="black", size=16, family="serif"),
+                        plot.subtitle = element_text(color="grey26", size=14, family="serif"),
+                        legend.text = element_text(color="black", size=12, family="serif"),
+                        text = element_text(color="black", size=14, family="serif")
+)
+
 # Average sentiment per day
 # VADER data preparation
 df_means_VADER <- VADER %>%
@@ -18,26 +37,11 @@ df_means_VADER <- VADER %>%
 # VADER visualisation over time
 df_means_VADER %>% 
   ggplot() +
-  geom_line(aes(x=date, y=mean_compound), col="dodgerblue4") +
+  geom_point(aes(x=date, y=mean_compound), col="dodgerblue4") +
   xlab("Date") +
   ylab("Sentiment") +
   scale_x_date(date_minor_breaks = "1 day") +
-  theme(axis.text = element_text(color="black", size=12, family="serif"),
-        axis.text.x = element_text(color="black", size=12, family="serif"),
-        axis.ticks.x = element_line(),
-        axis.ticks.y = element_line(),
-        axis.line.x = element_line(size=0.5, color="grey"),
-        axis.line.y = element_line(size=0.5, color="grey"),
-        panel.grid = element_line(color = "honeydew2",
-                                  size = 0.5,
-                                  linetype = 1),
-        panel.background = element_rect(fill="white"),
-        plot.margin = margin(10,10,10,10),
-        plot.title = element_text(color="black", size=16, family="serif"),
-        plot.subtitle = element_text(color="grey26", size=14, family="serif"),
-        legend.text = element_text(color="black", size=12, family="serif"),
-        text = element_text(color="black", size=14, family="serif")
-  )
+  custom_theme
 
 # Correct stock data for stock split in June 2022
 stock$value <- stock$close_data
@@ -50,22 +54,7 @@ stock %>%
   xlab("Date") +
   ylab("Value") +
   scale_x_date(date_minor_breaks = "1 day") +
-  theme(axis.text = element_text(color="black", size=12, family="serif"),
-        axis.text.x = element_text(color="black", size=12, family="serif"),
-        axis.ticks.x = element_line(),
-        axis.ticks.y = element_line(),
-        axis.line.x = element_line(size=0.5, color="grey"),
-        axis.line.y = element_line(size=0.5, color="grey"),
-        panel.grid = element_line(color = "honeydew2",
-                                  size = 0.5,
-                                  linetype = 1),
-        panel.background = element_rect(fill="white"),
-        plot.margin = margin(10,10,10,10),
-        plot.title = element_text(color="black", size=16, family="serif"),
-        plot.subtitle = element_text(color="grey26", size=14, family="serif"),
-        legend.text = element_text(color="black", size=12, family="serif"),
-        text = element_text(color="black", size=14, family="serif")
-  )
+  custom_theme
 
 # Merge stock values and sentiment scores for dates
 # Create stock df for merging
@@ -86,22 +75,9 @@ df %>%
   xlab("Date") +
   ylab("Value") +
   scale_x_date(date_minor_breaks = "1 day") +
-  theme(axis.text = element_text(color="black", size=12, family="serif"),
-        axis.text.x = element_text(color="black", size=12, family="serif"),
-        axis.ticks.x = element_line(),
-        axis.ticks.y = element_line(),
-        axis.line.x = element_line(size=0.5, color="grey"),
-        axis.line.y = element_line(size=0.5, color="grey"),
-        panel.grid = element_line(color = "honeydew2",
-                                  size = 0.5,
-                                  linetype = 1),
-        panel.background = element_rect(fill="white"),
-        plot.margin = margin(10,10,10,10),
-        plot.title = element_text(color="black", size=16, family="serif"),
-        plot.subtitle = element_text(color="grey26", size=14, family="serif"),
-        legend.text = element_text(color="black", size=12, family="serif"),
-        text = element_text(color="black", size=14, family="serif")
-  )
+  custom_theme
 
 # Calculate correlation between stock value and sentiment score
-cor(df$mean_compound, df$value, use="complete.obs")
+cor.test(df$mean_compound, df$value, use="complete.obs")
+
+plot(df$mean_compound, df$value)
