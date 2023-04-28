f6accc8804f55241dfc950fbcb23151e63afc2c7
simonweigold <simonw750@gmail.com>
2023-04-28 10:54:23 +0200

    stored visualisations to "outputs"-folder.

--- scripts/04 - analysis.R
+++ scripts/04 - analysis.R
@@ -35,27 +35,39 @@ df_means_VADER <- VADER %>%
   summarize(mean_compound = mean(compound))
 
 # VADER visualisation over time
-df_means_VADER %>% 
+a <- df_means_VADER %>% 
   ggplot() +
   geom_point(aes(x=date, y=mean_compound), col="dodgerblue4") +
   xlab("Date") +
   ylab("Sentiment") +
   scale_x_date(date_minor_breaks = "1 day") +
   custom_theme
-
+ggsave(a,
+       filename = "sentiment-per-day.png",
+       path = here::here("outputs"),
+       device = "png",
+       width = 6, height = 4, units = "in",
+       dpi = 600
+)
 # Correct stock data for stock split in June 2022
 stock$value <- stock$close_data
 stock$value[stock$date > '2022-06-03'] <- stock$value*20
 
 # Visualisation of stock data over time
-stock %>% 
+b <- stock %>% 
   ggplot() +
   geom_line(aes(x=date, y=value), col="dodgerblue4") +
   xlab("Date") +
   ylab("Value") +
   scale_x_date(date_minor_breaks = "1 day") +
   custom_theme
-
+ggsave(b,
+       filename = "stock-value-per-day.png",
+       path = here::here("outputs"),
+       device = "png",
+       width = 6, height = 4, units = "in",
+       dpi = 600
+)
 # Merge stock values and sentiment scores for dates
 # Create stock df for merging
 stock_join <- stock %>%
@@ -66,23 +78,38 @@ df_means_VADER$date <- as.Date(df_means_VADER$date)
 stock_join$date <- as.Date(stock_join$date)
 # Create df
 df <- right_join(df_means_VADER, stock_join)
+# Export df for report
+write.csv(df, here::here("final_df.csv"))
 
 # Visualisation of stock and sentiment data over time
-df %>% 
+c <- df %>% 
   ggplot() +
-  geom_line(aes(x=date, y=mean_compound), col="dodgerblue") +
+  geom_point(aes(x=date, y=mean_compound), col="dodgerblue", size = 0.75) +
   geom_line(aes(x=date, y=value/max(value)), col="dodgerblue4") +
   xlab("Date") +
-  ylab("Sentiment and Value") +
+  ylab("Value") +
   scale_x_date(date_minor_breaks = "1 day") +
   custom_theme
-
+ggsave(c,
+       filename = "stock-and-sentiment-per-day.png",
+       path = here::here("outputs"),
+       device = "png",
+       width = 6, height = 4, units = "in",
+       dpi = 600
+)
 # Visualise and calculate correlation between stock value and sentiment score
-df %>% 
+d <- df %>% 
   ggplot() +
   geom_point(aes(x=mean_compound, y=value, col = date)) +
   xlab("Sentiment Score") +
   ylab("Value") +
   custom_theme
+ggsave(d,
+       filename = "stock-and-sentiment-correlation.png",
+       path = here::here("outputs"),
+       device = "png",
+       width = 6, height = 4, units = "in",
+       dpi = 600
+)
 
 cor.test(df$mean_compound, df$value, use="complete.obs")
